0cf47cfacda8484260d9a5e504bb9afbccd20690
psmever <psmever@gmail.com>
2023-08-25 16:14:24 +0900

    build: helmet 추가

--- src/Pages/Bora/Main/MainPage.tsx
+++ src/Pages/Bora/Main/MainPage.tsx
@@ -1,5 +1,18 @@
+import { Helmet } from 'react-helmet-async';
+import Const from '@Const';
+import Detail from './Detail/Main';
+
 const MainPage = () => {
-    return <div className="flex flex-col h-screen w-full items-center justify-center">MainPage</div>;
+    return (
+        <>
+            <Helmet>
+                <title>{`${Const.Site.title} | 메인`}</title>
+                <meta name="description" content="메인 페이지" />
+            </Helmet>
+
+            <Detail />
+        </>
+    );
 };
 
 export default MainPage;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import '@Style/GlobalStyles.css';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.Fragment>
-        <App />
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
     </React.Fragment>
 );
