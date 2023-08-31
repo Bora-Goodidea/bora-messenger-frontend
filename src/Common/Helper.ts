703a039aeaab3792566d6d329f29cc3e63679571
psmever <psmever@gmail.com>
2023-08-31 17:11:34 +0900

    build: helper 업데이트

--- src/Common/Helper.ts
+++ src/Common/Helper.ts
@@ -32,3 +32,136 @@ export const colorDebug = (color: 'success' | 'info' | 'error' | 'warning', mess
 export const DEBUG = (e: any) => {
     console.debug('%c::DEBUG::', 'color: green; font-weight: bold;', e);
 };
+
+/**
+ * localStorage 체크
+ */
+export const isLocalStorageEnabled = (): boolean => {
+    try {
+        const key = `__storage__test`;
+        window.localStorage.setItem(key, '');
+        window.localStorage.removeItem(key);
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+/**
+ * 로컬 스토리지 매니저.
+ */
+export const storageManager = {
+    set: (key: string, object: any) => {
+        if (!localStorage) return;
+        localStorage[key] = typeof object === 'string' ? object : JSON.stringify(object);
+    },
+    get: (key: string) => {
+        if (!localStorage) return null;
+
+        if (!localStorage[key]) {
+            return null;
+        }
+
+        try {
+            return JSON.parse(localStorage[key]);
+        } catch (e) {
+            return localStorage[key];
+        }
+    },
+    remove: (key: string) => {
+        if (!localStorage) return null;
+
+        if (localStorage[key]) {
+            localStorage.removeItem(key);
+        }
+    },
+};
+
+/**
+ * 로컬 쿠키 매니저.
+ */
+export const cookieManager = {
+    set: (cname: string, cvalue: string, hours = 24) => {
+        const d = new Date();
+        d.setTime(d.getTime() + hours * 60 * 60 * 1000); // (exdays * 24 * 60 * 60 * 1000));
+        const expires = 'expires=' + d.toUTCString();
+        document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+    },
+    get: (cname: string) => {
+        const name = cname + '=';
+        const ca = document.cookie.split(';');
+
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+
+        return '';
+    },
+    remove: (cname: string) => {
+        const expires = 'expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+        document.cookie = cname + '=;' + expires + ';path=/';
+    },
+};
+
+/**
+ * localstorage, cookie 매니져
+ */
+export const storageMaster = {
+    set: (key: string, object: any) => {
+        if (isLocalStorageEnabled()) {
+            storageManager.set(key, object);
+        } else {
+            cookieManager.set(key, object);
+        }
+    },
+    get: (key: string) => {
+        if (isLocalStorageEnabled()) {
+            return storageManager.get(key);
+        } else {
+            return cookieManager.get(key);
+        }
+    },
+    remove: (key: string) => {
+        if (isLocalStorageEnabled()) {
+            storageManager.remove(key);
+        } else {
+            cookieManager.remove(key);
+        }
+    },
+};
+
+/**
+ * RefreshToken 저장
+ * @param accessToken
+ * @param refreshToken
+ */
+export const saveRefreshToken = ({ accessToken, refreshToken }: { accessToken: string; refreshToken: string }): void => {
+    storageMaster.set(`accessToken`, accessToken);
+    storageMaster.set(`refreshToken`, refreshToken);
+
+    return;
+};
+
+/**
+ * RefreshToken 삭제
+ */
+export const removeLoginToken = (): void => {
+    storageMaster.remove(`accessToken`);
+    storageMaster.remove(`refreshToken`);
+};
+
+/**
+ * RefreshToken 리턴
+ */
+export const getRefreshToken = (): string => storageMaster.get(`refreshToken`);
+
+/**
+ * AccessToken 저장
+ */
+export const getAccessToken = (): string => storageMaster.get(`accessToken`);
