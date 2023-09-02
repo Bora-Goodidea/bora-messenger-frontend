0d2e4146ab9998061a5e37ef4111a337fd452317
psmever <psmever@gmail.com>
2023-09-02 18:14:42 +0900

    build: main page helmet 변경

--- src/Pages/Bora/Messenger/MessengerPage.tsx
+++ src/Pages/Bora/Messenger/MessengerPage.tsx
@@ -2,12 +2,14 @@ import { Helmet } from 'react-helmet-async';
 import Const from '@Const';
 import MainContainer from './Detail/MainContainer';
 
+const pageName = `회원가입`;
+
 const MessengerPage = () => {
     return (
         <>
             <Helmet>
-                <title>{`${Const.Site.title} | 메인`}</title>
-                <meta name="description" content="메인 페이지" />
+                <title>{`${Const.Site.title} | ${pageName}`}</title>
+                <meta name="description" content={`${pageName} 페이지`} />
             </Helmet>
 
             <MainContainer />
