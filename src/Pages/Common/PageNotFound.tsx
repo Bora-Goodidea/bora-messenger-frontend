9d1bacf09d76b10083002e22edab9d0e0a3695b9
psmever <psmever@gmail.com>
2023-09-18 10:26:01 +0900

    build: under-construction page 추가

--- src/Pages/Common/PageNotFound.tsx
+++ src/Pages/Common/PageNotFound.tsx
@@ -1,35 +1,46 @@
 import { useNavigate } from 'react-router-dom';
 import { ExclamationMarkIcon, BackIcon as BackIconSvg } from '@Icons';
 import { LayoutStyles } from '@Styles';
+import { Helmet } from 'react-helmet-async';
+import Const from '@Const';
 
 const { MainContainer } = LayoutStyles.DafalutLayoutStyle;
 const { Section, Container, Wapper, IconWapper, TitleText, SubText, BackIcon, BackButton } = LayoutStyles.NotFoundPageStyle;
+
+const pageName = `존재 하지 않은`;
 
 const PageNotFound = () => {
     const navigate = useNavigate();
 
     return (
-        <MainContainer>
-            <Section>
-                <Container>
-                    <Wapper>
-                        <IconWapper>
-                            <ExclamationMarkIcon />
-                        </IconWapper>
-                        <TitleText>페이지가 존재 하지 않아요</TitleText>
-                        <SubText>찾고 계신 페이지가 존재하지 않습니다.</SubText>
+        <>
+            <Helmet>
+                <title>{`${Const.Site.title} | ${pageName}`}</title>
+                <meta name="description" content={`${pageName} 페이지`} />
+            </Helmet>
 
-                        <BackIcon onClick={() => navigate(`${process.env.PUBLIC_URL}/`)}>
-                            <BackButton>
-                                <BackIconSvg />
+            <MainContainer>
+                <Section>
+                    <Container>
+                        <Wapper>
+                            <IconWapper>
+                                <ExclamationMarkIcon />
+                            </IconWapper>
+                            <TitleText>페이지가 존재 하지 않아요</TitleText>
+                            <SubText>찾고 계신 페이지가 존재하지 않습니다.</SubText>
 
-                                <span>뒤로</span>
-                            </BackButton>
-                        </BackIcon>
-                    </Wapper>
-                </Container>
-            </Section>
-        </MainContainer>
+                            <BackIcon onClick={() => navigate(`${process.env.PUBLIC_URL}/`)}>
+                                <BackButton>
+                                    <BackIconSvg />
+
+                                    <span>뒤로</span>
+                                </BackButton>
+                            </BackIcon>
+                        </Wapper>
+                    </Container>
+                </Section>
+            </MainContainer>
+        </>
     );
 };
 
